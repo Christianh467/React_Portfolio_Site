00c2309bc83e20941477780eb390eb3775905259
Christian Head <christianhead467@gmail.com>
2022-12-11 21:08:30 -0600

    Added Sections as well as their titles and css

--- src/components/portfolio-site.js
+++ src/components/portfolio-site.js
@@ -3,11 +3,44 @@ import '../styles/portfolio-site.css';
 function Portfolio_Site() {
   return (
     <div className="website">
-      <nav>
-        <div className='navbar'>
+      <div id='header' className='header'>
+        <ul className="nav">
+          <a className="nav-link" href="#">
+            <li className="nav-item">Intro</li>
+          </a>
+          <a className="nav-link" href="#">
+            <li className="nav-item">Resume</li>
+          </a>
+          <a className="nav-link" href="#">
+            <li className="nav-item">Portfolio</li>
+          </a>
+          <a className="nav-link" href="#">
+            <li className="nav-item">Contact</li>
+          </a>
+        </ul>
+      </div>
+      <div id='welcome-section'>
+        <h1>Hey, I am Christian Head</h1>
+        <h3>front-end engineer, future full-stack</h3>
+      </div>
+      <div id='resume-section'>
+        <a href='something'>
+          <i id='resume-icon'></i>
+        </a>
+      </div>
+      <div id='project-section'>
+        <h2>These are some of my projects</h2>
+      </div>
+      <div id='contact-section'>
+        <h1>Lets Work Together...</h1>
+        <p>the future is waiting</p>
+        <div id='contact-nav'>
 
         </div>
-      </nav>
+      </div>
+      <div id='footer'>
+        <span>This webpage acts as my portfolio and resume. If you still have any further questions or concerns, please feel free to contact me.</span>
+      </div>
     </div>
   );
 }
