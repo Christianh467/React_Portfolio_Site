d63d59c1cca579f484ec29df965aa523589aceaf
Christian Head <christianhead467@gmail.com>
2022-12-12 14:50:20 -0600

    Added the projects section   - added project imgs folder in public folder   - added row/grid format within project section   - made each project a .card class with responsive sizing

--- src/components/portfolio-site.js
+++ src/components/portfolio-site.js
@@ -33,7 +33,120 @@ function Portfolio_Site() {
       <div id='project-section'>
         <h2>These are some of my projects</h2>
         <div>
-          carousel of projects
+          <div id='project-grid' className='row gy-4 gx-5'>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/React_Pomodoro_Clock/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Pomodoro Clock</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/pomodoro_clock.png' alt='Pomodoro_Clock_Project'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/React_Calculator/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Calculator</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/calculator.png' alt='Calculator_Project'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/React_Drumpad/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Drumpad</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/drumpad.png' alt='Drumpad_Project'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/React_Markdown_Previewer/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Markdown Previewer</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/markdown_reviewer.png' alt='Markdown_Previewer_Project'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/React_Quote_Generator/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Quote Generator</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/quote_generator.png' alt='Quote_Generator_Project'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/AnimationFerrisWheel/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Ferris Wheel Animation</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/AnimationFerrisWheel.jpg' alt='Animated_Ferris_Wheel'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/BalanceSheet/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Balance Sheet Template</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/BalanceSheetTemplate.jpg' alt='Balance_Sheet_Template'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/CafeMenu/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Cafe Menu Template</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/CafeMenuTemplate.jpg' alt='Cafe_Menu_Template'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/CitySkyline/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Responsive City Skyline</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/CitySkylinesScenery.jpg' alt='Responsive_City_Skyline'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/NutritionLabel/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Nutrition Label Template</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/NutritionLabelTemplate.jpg' alt='Nutritional_Label_Template'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/PenguinTransforms/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Penguin Transforms</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/PenguinTransforms.jpg' alt='Penguin_Waving'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/ProductLandingPage/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Product Landing Page</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/ProductLandingPage.jpg' alt='Product_Landing_Page_Template'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/Survey_Form/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Survey Form Template</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/SurveyFormTemplate.jpg' alt='Survey_Form_Template'></img>
+              </a>
+            </div>
+            <div className='col-sm-6 col-lg-4'>
+              <a className='card p-3' href='https://christianh467.github.io/TributePage/' target='_blank'>
+                <div className='card-body'>
+                  <h4 className='card-title'>Tribute Page</h4>
+                </div>
+                <img className='card-img-bottom' src='PortfolioSiteImgs/tribute-page-main-image.jpg' alt='Tribute_Page_Template'></img>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div id='contact-section'>
