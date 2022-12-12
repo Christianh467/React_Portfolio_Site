f5cf54f5f4a02a00f1cb519aeedfeb94e91d4438
Christian Head <christianhead467@gmail.com>
2022-12-11 19:01:42 -0600

    setting up webpage

---
+++ src/components/portfolio-site.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+
+function Portfolio_Site() {
+  return (
+    <div className="website">
+      <nav>
+        <div className='navbar'>
+
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Portfolio_Site;
