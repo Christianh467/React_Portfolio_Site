0211e6d9c2923304515d8a276cee03272b90757d
Christian Head <christianhead467@gmail.com>
2022-12-11 21:59:56 -0600

    Added references to nav links in header - had to add an additional reference div inside #welcome-section to make the jump to #resume-section accurate

--- src/components/portfolio-site.js
+++ src/components/portfolio-site.js
@@ -5,22 +5,23 @@ function Portfolio_Site() {
     <div className="website">
       <div id='header' className='header'>
         <ul className="nav">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#welcome-section">
             <li className="nav-item">Intro</li>
           </a>
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#resume-reference">
             <li className="nav-item">Resume</li>
           </a>
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#project-section">
             <li className="nav-item">Portfolio</li>
           </a>
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#contact-section">
             <li className="nav-item">Contact</li>
           </a>
         </ul>
       </div>
       <div id='welcome-section'>
         <h1>Hey, I am Christian Head</h1>
+        <div id='resume-reference'></div>
         <h3>front-end engineer, future full-stack</h3>
       </div>
       <div id='resume-section'>
@@ -35,8 +36,11 @@ function Portfolio_Site() {
       <div id='contact-section'>
         <h1>Lets Work Together...</h1>
         <p>the future is waiting</p>
-        <div id='contact-nav'>
-
+        <div class='contact-list'>
+          <li class='profile-link'><div class='img-container'><a id='profile-link' href='https://www.facebook.com/Christianh467' target='_blank'><i class="fa-brands fa-square-facebook"></i><span>Facebook</span></a></div></li>
+          <li class='profile-link'><div class='img-container'><a id='profile-link' href='https://github.com/Christianh467' target='_blank'><i class="fa-brands fa-github"></i><span>Github</span></a></div></li>
+          <li class='profile-link'><div class='img-container'><a id='profile-link' href=' ' target='_blank'><i class="fa-solid fa-mobile"></i><span id='phone'>(817)-939-7721</span></a></div></li>
+          <li class='profile-link'><div class='img-container'><a id='profile-link' href=' ' target='_blank'><i class="fa-solid fa-paper-plane"></i><span id='email'>Christianhead467@gmail.com</span></a></div></li>
         </div>
       </div>
       <div id='footer'>
